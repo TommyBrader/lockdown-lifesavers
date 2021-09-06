94479758d84f92134b00ad7dfe00d516402aa6ef
Tommy Brader <tj.brader98@gmail.com>
2021-09-06 21:07:22 +0100

    change battleships

---
+++ sections/games/scripts/battleships-script.js
@@ -0,0 +1,5 @@
+console.log('connected')
+
+const messageArea = document.querySelector('#message')
+const fireButton = document.querySelector('#fire-button')
+const guessInput = document.querySelector('#guess')
