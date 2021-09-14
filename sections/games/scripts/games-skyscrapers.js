c95308578ffcacd6fab9298f9664ec7c74822353
Tommy Brader <tj.brader98@gmail.com>
2021-09-14 02:02:23 +0100

    add functionality to skyscrapers and most checks

--- sections/games/scripts/games-skyscrapers.js
+++ sections/games/scripts/games-skyscrapers.js
@@ -9,6 +9,7 @@ const submitAttempt = document.querySelector('#submit-button')
 
 let numOfAttempts = 0
 let index
+let indexToFind
 
 // Grids
 const grids = [
@@ -64,6 +65,7 @@ let thirdSolutionColumn = [t0l2, t1l2, t2l2, t3l2]
 let fourthSolutionColumn = [t0l3, t1l3, t2l3, t3l3]
 
 let solution = [firstSolutionRow, secondSolutionRow, thirdSolutionRow, fourthSolutionRow]
+let columnSolution = [firstSolutionColumn, secondSolutionColumn, thirdSolutionColumn, fourthSolutionColumn]
 
 let fullGuess
 
@@ -106,6 +108,7 @@ function generateClues() {
   //   leftClue[3].textContent = 1
   // }
 
+  // Initial Affect on solution array by clues
   for (let i=0; i<4; i++) {
     // If clue is 4
     if (topClue[i].textContent == 4) {
@@ -258,6 +261,163 @@ function generateClues() {
       index = fourthSolutionRow[i].indexOf(3)
       if (index > -1) {fourthSolutionRow[i].splice(index, 1)}
     }
+
+    // If Clue is 2
+    if (topClue[i].textContent == 2) {
+      index = firstSolutionRow[i].indexOf(4)
+      if (index > -1) {firstSolutionRow[i].splice(index, 1)}
+    }
+    if (leftClue[i].textContent == 2) {
+      index = firstSolutionColumn[i].indexOf(4)
+      if (index > -1) {firstSolutionColumn[i].splice(index, 1)}
+    }
+    if (rightClue[i].textContent == 2) {
+      index = fourthSolutionColumn[i].indexOf(4)
+      if (index > -1) {fourthSolutionColumn[i].splice(index, 1)}
+    }
+    if (bottomClue[i].textContent == 2) {
+      index = fourthSolutionRow[i].indexOf(4)
+      if (index > -1) {fourthSolutionRow[i].splice(index, 1)}
+    }
+
+    // If Clue is 3
+    if (topClue[i].textContent == 3) {
+      index = firstSolutionRow[i].indexOf(3)
+      if (index > -1) {firstSolutionRow[i].splice(index, 1)}
+      index = firstSolutionRow[i].indexOf(4)
+      if (index > -1) {firstSolutionRow[i].splice(index, 1)}
+      index = secondSolutionRow[i].indexOf(4)
+      if (index > -1) {secondSolutionRow[i].splice(index, 1)}
+    }
+    if (leftClue[i].textContent == 3) {
+      index = firstSolutionColumn[i].indexOf(3)
+      if (index > -1) {firstSolutionColumn[i].splice(index, 1)}
+      index = firstSolutionColumn[i].indexOf(4)
+      if (index > -1) {firstSolutionColumn[i].splice(index, 1)}
+      index = secondSolutionColumn[i].indexOf(4)
+      if (index > -1) {secondSolutionColumn[i].splice(index, 1)}
+    }
+    if (rightClue[i].textContent == 3) {
+      index = fourthSolutionColumn[i].indexOf(3)
+      if (index > -1) {fourthSolutionColumn[i].splice(index, 1)}
+      index = fourthSolutionColumn[i].indexOf(4)
+      if (index > -1) {fourthSolutionColumn[i].splice(index, 1)}
+      index = thirdSolutionColumn[i].indexOf(4)
+      if (index > -1) {thirdSolutionColumn[i].splice(index, 1)}
+    }
+    if (bottomClue[i].textContent == 3) {
+      index = fourthSolutionRow[i].indexOf(3)
+      if (index > -1) {fourthSolutionRow[i].splice(index, 1)}
+      index = fourthSolutionRow[i].indexOf(4)
+      if (index > -1) {fourthSolutionRow[i].splice(index, 1)}
+      index = thirdSolutionRow[i].indexOf(4)
+      if (index > -1) {thirdSolutionRow[i].splice(index, 1)}
+    }
+
+    // Need to iterate over statements multiple times, arbitrary 100 iterations selected
+    let count = 0
+    while (count < 100) {
+      // Remove possibilities where value has been found in other cell in row/column
+      for (let i=0; i<4; i++) {
+        for (let j=0; j<4; j++) {
+          if (columnSolution[i][j].length == 1) {
+            for (let k=0; k<4; k++) {
+              indexToFind = parseInt(columnSolution[i][j][0])
+              if (typeof columnSolution[i][k] !== 'undefined') {
+                index = columnSolution[i][k].indexOf(indexToFind)
+                if (index > -1 && columnSolution[i][k].length != 1) {
+                  columnSolution[i][k].splice(index, 1)
+                }
+              }
+            }
+          }
+        }
+      }
+      for (let i=0; i<4; i++) {
+        for (let j=0; j<4; j++) {
+          if (solution[i][j].length == 1) {
+            for (let k=0; k<4; k++) {
+              indexToFind = parseInt(solution[i][j][0])
+              if (typeof solution[i][k] !== 'undefined') {
+                index = solution[i][k].indexOf(indexToFind)
+                if (index > -1 && solution[i][k].length != 1) {
+                  solution[i][k].splice(index, 1)
+                }
+              }
+            }
+          }
+        }
+      }
+
+      // Finds any cell that must equal a certain value based on other posibility lists
+      for (let i=0; i<4; i++) {
+        for (let j=0; j<4; j++) {
+          columnSolution[i][j].forEach(value => {
+              let valueArray = [0,1,2,3].splice([0,1,2,3].indexOf(value), 1)
+              let colSolValArr0
+              let colSolValArr1
+              let colSolValArr2
+              if (columnSolution[i][valueArray[0]] != undefined) {
+                colSolValArr0 = columnSolution[i][valueArray[0]].indexOf(value)
+              } else {
+                colSolValArr0 = 0
+              }
+              if (columnSolution[i][valueArray[1]] != undefined) {
+                colSolValArr1 = columnSolution[i][valueArray[1]].indexOf(value)
+              } else {
+                colSolValArr1 = 0
+              }
+              if (columnSolution[i][valueArray[2]] != undefined) {
+                colSolValArr2 = columnSolution[i][valueArray[2]].indexOf(value)
+              } else {
+                colSolValArr2 = 0
+              }
+                if (colSolValArr0 == -1 && colSolValArr1 == -1 && colSolValArr2 == -1) {
+                  index = columnSolution[i][j].indexOf(valueArray[0])
+                  if (index > -1) {columnSolution.splice(index, 1)}
+                  index = columnSolution[i][j].indexOf(valueArray[1])
+                  if (index > -1) {columnSolution.splice(index, 1)}
+                  index = columnSolution[i][j].indexOf(valueArray[2])
+                  if (index > -1) {columnSolution.splice(index, 1)}
+                }
+          })
+        }
+      }
+      for (let i=0; i<4; i++) {
+        for (let j=0; j<4; j++) {
+          solution[i][j].forEach(value => {
+              let valueArray = [0,1,2,3].splice([0,1,2,3].indexOf(value), 1)
+              let solValArr0
+              let solValArr1
+              let solValArr2
+              if (solution[i][valueArray[0]] != undefined) {
+                solValArr0 = solution[i][valueArray[0]].indexOf(value)
+              } else {
+                solValArr0 = 0
+              }
+              if (solution[i][valueArray[1]] != undefined) {
+                solValArr1 = solution[i][valueArray[1]].indexOf(value)
+              } else {
+                solValArr1 = 0
+              }
+              if (solution[i][valueArray[2]] != undefined) {
+                solValArr2 = solution[i][valueArray[2]].indexOf(value)
+              } else {
+                solValArr2 = 0
+              }
+                if (solValArr0 == -1 && solValArr1 == -1 && solValArr2 == -1) {
+                  index = solution[i][j].indexOf(valueArray[0])
+                  if (index > -1) {solution.splice(index, 1)}
+                  index = solution[i][j].indexOf(valueArray[1])
+                  if (index > -1) {solution.splice(index, 1)}
+                  index = solution[i][j].indexOf(valueArray[2])
+                  if (index > -1) {solution.splice(index, 1)}
+                }
+          })
+        }
+      }
+      count++
+    }
   }
 
   console.log(solution)
@@ -268,6 +428,7 @@ submitAttempt.addEventListener('click', submitClicked)
 
 function submitClicked(event) {
   event.preventDefault()
+  console.log('submit')
   const inputGuess = document.querySelectorAll('.guess-input')
   messageArea.textContent = ''
   let checkGuess = true
@@ -281,10 +442,10 @@ function submitClicked(event) {
     }
   })
   if (checkGuess) {
-    const firstRowInput = [[inputGuess[0]], [inputGuess[1]], [inputGuess[2]], [inputGuess[3]]]
-    const secondRowInput = [[inputGuess[4]], [inputGuess[5]], [inputGuess[6]], [inputGuess[7]]]
-    const thirdRowInput = [[inputGuess[8]], [inputGuess[9]], [inputGuess[10]], [inputGuess[11]]]
-    const fourthRowInput = [[inputGuess[12]], [inputGuess[13]], [inputGuess[14]], [inputGuess[15]]]
+    const firstRowInput = [[parseInt(inputGuess[0].value)], [parseInt(inputGuess[1].value)], [parseInt(inputGuess[2].value)], [parseInt(inputGuess[3].value)]]
+    const secondRowInput = [[parseInt(inputGuess[4].value)], [parseInt(inputGuess[5].value)], [parseInt(inputGuess[6].value)], [parseInt(inputGuess[7].value)]]
+    const thirdRowInput = [[parseInt(inputGuess[8].value)], [parseInt(inputGuess[9].value)], [parseInt(inputGuess[10].value)], [parseInt(inputGuess[11].value)]]
+    const fourthRowInput = [[parseInt(inputGuess[12].value)], [parseInt(inputGuess[13].value)], [parseInt(inputGuess[14].value)], [parseInt(inputGuess[15].value)]]
 
     // const firstColumnInput = {0: inputGuess[0], 1: inputGuess[4], 2: inputGuess[8], 3: inputGuess[12]}
     // const secondColumnInput = {0: inputGuess[1], 1: inputGuess[5], 2: inputGuess[9], 3: inputGuess[13]}
@@ -294,8 +455,10 @@ function submitClicked(event) {
     fullGuess = [firstRowInput, secondRowInput, thirdRowInput, fourthRowInput]
   }
   numOfAttempts++
-  if (fullGuess == solution) {
-    messageArea.textContent = `Congratulations! You have completed the board in ${numOfAttempts} guesses`
+  console.log(fullGuess)
+  console.log(solution)
+  if (JSON.stringify(fullGuess) == JSON.stringify(solution)) {
+    messageArea.textContent = `Congratulations! You have completed the board in ${numOfAttempts} guess(es)`
   }
 }
 
